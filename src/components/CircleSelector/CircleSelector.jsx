8b1930506cec721ac17aa64ee4d96fedc37febe8
Katherine Wong <katherinewongkw3@gmail.com>
2021-08-07 11:47:38 -0700

    creates react app and app components

---
+++ src/components/CircleSelector/CircleSelector.jsx
@@ -0,0 +1,10 @@
+
+// function CircleSelector 
+
+
+
+
+
+
+
+// The <CircleSelector> component can be defined as a Function Component and is responsible for letting <App> know that the user wants to select a certain circle in the <Circles> component.
